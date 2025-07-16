bd4ecfaf130832c2cc658f6584c10ca276391cef
peaklabs-dev <122374094+peaklabs-dev@users.noreply.github.com>
2025-07-16 16:59:42 +0200

    feat(core): create user, job and password reset tables

---
+++ app/Models/User.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Models;
+
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+final class User extends Authenticatable
+{
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var list<string>
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    /**
+     * Get the attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
+    {
+        return [
+            'email_verified_at' => 'datetime',
+            'password' => 'hashed',
+        ];
+    }
+}
