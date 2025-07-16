461882acf4512a6887b050a2359514a7dee345d8
peaklabs-dev <122374094+peaklabs-dev@users.noreply.github.com>
2025-07-16 17:09:44 +0200

    chore(core): formatting

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,15 @@
 import { defineConfig } from 'vite';
-import tailwindcss from '@tailwindcss/vite';
 import laravel from 'laravel-vite-plugin';
+import tailwindcss from '@tailwindcss/vite';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
     plugins: [
-        tailwindcss(),
         laravel({
-            input: ['resources/js/app.js', 'resources/css/app.css'],
+            input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
+        tailwindcss(),
         vue(),
     ],
 });
