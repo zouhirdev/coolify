29f389a672fb54dfdc02a059a7460fcf25576cf0
peaklabs-dev <122374094+peaklabs-dev@users.noreply.github.com>
2025-08-14 14:40:27 +0200

    feat: new coolify description and keywords

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,6 +5,7 @@
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta name="robots" content="noindex">
+    <meta name="description" content="Coolify: An open-source & self-hostable alternative to Vercel, Heroku, Netlify and Railway for easily deploying websites, databases, web applications and 280+ one-click services to your own server.">
     <title>Coolify</title>
     @routes
     @vite(['resources/css/app.css', 'resources/js/app.js'])
