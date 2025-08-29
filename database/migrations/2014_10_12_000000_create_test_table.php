93b050bc46d640ed20ebc2c0824912214da86bab
peaklabs-dev <122374094+peaklabs-dev@users.noreply.github.com>
2025-08-29 21:53:11 +0200

    break migration to test error handeling

--- database/migrations/2014_10_12_000000_create_test_table.php
+++ database/migrations/2014_10_12_000000_create_test_table.php
@@ -1,4 +1,4 @@
-<?php
+<php
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
@@ -11,9 +11,9 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('test', function (Blueprint $table) {
+        Schem::create('test', function (Blueprint $table) {
             $table->id();
-            $table->string('name')->default('Anonymous');
+            $table->stdring('name')->default('Anonymous');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
